0ff1a5e5c4b88fb250c8834cc1abe41e95f05d23
Sahar Yusufzai <syusufza@my.centennialcollege.ca>
2022-10-15 12:25:27 +0500

    Database error resolution p2

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://m001-student:Ibtisam@sandbox.xuwkkn8.mongodb.net/Portfolio?retryWrites=true&w=majority').then(() => {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://m001-student:Ibtisam@sandbox.xuwkkn8.mongodb.net/Portfolio?retryWrites=true&w=majority').then(() => {
     console.log('Database Connection successful');
 }).catch(err => {
     console.log(err)
