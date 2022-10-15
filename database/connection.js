6b4c049a23c28a89eee5c3b7f891d25f1f3d3eee
Sahar Yusufzai <syusufza@my.centennialcollege.ca>
2022-10-15 11:27:10 +0500

    Database connection parameter changed

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://m001-student:Ibtisam@sandbox.xuwkkn8.mongodb.net/Portfolio?retryWrites=true&w=majority').then(() => {
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://m001-student:Ibtisam@sandbox.xuwkkn8.mongodb.net/Portfolio?retryWrites=true&w=majority').then(() => {
     console.log('Database Connection successful');
 }).catch(err => {
     console.log(err)
